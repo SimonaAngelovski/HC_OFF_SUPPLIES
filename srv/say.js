111044229a443a075282818f6563dde6fa17ee26
Simona Angelovski <simona.angelovski@gmail.com>
2023-11-08 11:58:56 +0000

    hello wrold and JS handler

---
+++ srv/say.js
@@ -0,0 +1,7 @@
+// module.exports = class say {  
+//     hello(req) { return `Hello ${req.data.to}!` }
+// }
+
+module.exports = (say) =>{  
+    say.on ('hello',req => `Hello ${req.data.to}!` )
+}
